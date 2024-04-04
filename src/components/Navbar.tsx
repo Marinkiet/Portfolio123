2989b44d2c8644ef2ccdbb7edd0507d8f923a5e5
Marinkie <tmarinkie12@gmail.com>
2024-04-05 00:59:13 +0200

    Fix: chat link removed

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,16 +1,15 @@
 import Link from "next/link";
 
-export default function Navbar (){
+export default function Navbar() {
     return (
         <header className=" bg-background sticky">
-            <div className="max-w-3xl flex flex-wrap gap-2 px-3 py-4 items-center justify-center">
-               <nav className="space-x-4 font-sans text-gray-400">
-                <Link className="hover:text-purple-400 hover:cursor-pointer" href="/">home</Link>
-                <Link className="hover:text-purple-400 hover:cursor-pointer" href="/about">about</Link>
-                <Link className="hover:text-purple-400 hover:cursor-pointer" href="/chat">chatBot</Link>
-                <Link className="hover:text-yellow-500 hover:cursor-pointer"  href="/certifications">Certifications</Link>       
-                <Link className="hover:text-blue-400 hover:cursor-pointer" target="_blank" href="https://www.linkedin.com/in/marinkie-thupi/">Linkedin</Link>
-               </nav>
+            <div className="max-w-3xl flex flex-wrap gap-2 px-3 py-4 items-center">
+                <nav className="space-x-4 font-sans text-gray-400 items-center">
+                    <Link className="hover:text-purple-400 hover:cursor-pointer" href="/">home</Link>
+                    <Link className="hover:text-purple-400 hover:cursor-pointer" href="/about">about</Link>
+                    <Link className="hover:text-yellow-500 hover:cursor-pointer" href="/certifications">certifications</Link>
+                    <Link className="hover:text-blue-400 hover:cursor-pointer" target="_blank" href="https://www.linkedin.com/in/marinkie-thupi/">Linkedin</Link>
+                </nav>
             </div>
         </header>
     )
