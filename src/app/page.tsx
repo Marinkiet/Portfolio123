0380595c0c5e1a8161db8693092c651884f4f334
Marinkie <tmarinkie12@gmail.com>
2024-04-05 01:03:23 +0200

    Feat: Added chatbot heading

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,6 +7,8 @@ import me from '../assets/me.jpeg';
 import Navbar from "@/components/Navbar";
 import ProjectButton from "@/components/UI/ProjectButton";
 import ThemeToggle from "@/components/UI/ThemeToggle";
+import AIChatButton from "@/components/UI/AIChatButton";
+import { Bot } from "lucide-react";
 
 //page specifuic metadata
 export const metadata: Metadata = {
@@ -15,46 +17,46 @@ export const metadata: Metadata = {
 
 export default function Home() {
   return (
-    
+
     <section className="space-y-16 px-1 py-8 flex justify-center align-middle flex-col sm:flex-row-reverse sm:justify-between">
       {/* for mobile set 1 column then 670px and obove set 2 column layout  */}
-      <section>
-      <ThemeToggle/>
+      <section className=" items-center">
+        <ThemeToggle />
+
       </section>
+
       <section className="grid-cols-1 items-center gap-8 sm:grid-cols-2">
         <div className="  space-y-3 mb-5  bg-[url('/background.png')] bg-no-repeat bg-cover bg-center">
-      
-          <H1 className="  text-center sm:text-start">Hello, I&apos;m Marinkie Thupi</H1>
-
-          <p className="  text-center sm:text-start">Full Stack Developer: No layer left <span className="text-purple-500">unturned.</span></p>
-          
+          <H1 className="  text-center sm:text-start">Hello, I&apos;m Marinkie Thupi </H1>
+          <p className=" text-center sm:text-start">Full Stack Developer: No layer left <span className="text-purple-500">unturned.</span></p>
           <Navbar />
         </div>
-      </section>
-      {/* <section> */}
-        {/* <video className="w-full h-full sm:w-auto object-coverrounded-lg shadow-md" loop muted autoPlay playsInline>
-          <source src="../assets/video.mp4" type="video/mp4" />
-          {/* Your browser does not support the video tag. 
-        </video> */}
+        <section className="items-center flex gap-2">
+          <AIChatButton/>
+        <h2>ask me anything.</h2>
+        </section>
 
-      {/* </section> */}
+      </section>
       <section> {/* Scrollable section */}
         <section className="space-y-3 items-center"> {/* Nested section for list items */}
-          <ProjectButton projectLink="/" projectName="Portfolio" projectYear="2024" githubLink=""/>
-          <ProjectButton projectLink="/partyrock" projectName="AWS AI/PartyRock App" projectYear="2024" githubLink="https://github.com/Marinkiet/PartyRockAPPDemo"/>
-          <ProjectButton projectLink="/neumorphism" projectName="UI/Neumorphism " projectYear="2024" githubLink=""/>
-          <ProjectButton projectLink="/onboarding" projectName="UI/Onboarding" projectYear="2024" githubLink=""/>
-          <ProjectButton projectLink="/codepencil" projectName="Clone/CodePencil" projectYear="2024" githubLink=""/>
-          <ProjectButton projectLink="/habittracker" projectName="Habit Tracker App" projectYear="2024" githubLink=""/>
-          <ProjectButton projectLink="/partyrock" projectName="Job Search App" projectYear="2023" githubLink=""/>
-          <ProjectButton projectLink="/partyrock" projectName="UI/Shopping Website" projectYear="2022" githubLink=""/>
-          <ProjectButton projectLink="/partyrock" projectName="Tap Sound Player" projectYear="2022" githubLink=""/>
-          <ProjectButton projectLink="/partyrock" projectName="Clone/Google Search" projectYear="2021" githubLink=""/>
-          <ProjectButton projectLink="/partyrock" projectName="Clone/Tesla" projectYear="2021" githubLink=""/>
-          <ProjectButton projectLink="/partyrock" projectName="Clone/Instagram" projectYear="2021" githubLink=""/>
-          <ProjectButton projectLink="/partyrock" projectName="View More On Github..."  githubLink="https://github.com/Marinkiet?tab=repositories"/>
+
+          <ProjectButton projectLink="/" projectName="Portfolio" projectYear="2024" githubLink="" />
+          <ProjectButton projectLink="/partyrock" projectName="AWS AI/PartyRock App" projectYear="2024" githubLink="https://github.com/Marinkiet/PartyRockAPPDemo" />
+          <ProjectButton projectLink="/neumorphism" projectName="UI/Neumorphism " projectYear="2024" githubLink="" />
+          <ProjectButton projectLink="/onboarding" projectName="UI/Onboarding" projectYear="2024" githubLink="" />
+          <ProjectButton projectLink="/codepencil" projectName="Clone/CodePencil" projectYear="2024" githubLink="" />
+          <ProjectButton projectLink="/habittracker" projectName="Habit Tracker App" projectYear="2024" githubLink="" />
+          <ProjectButton projectLink="/partyrock" projectName="Job Search App" projectYear="2023" githubLink="" />
+          <ProjectButton projectLink="/partyrock" projectName="UI/Shopping Website" projectYear="2022" githubLink="" />
+          <ProjectButton projectLink="/partyrock" projectName="Tap Sound Player" projectYear="2022" githubLink="" />
+          <ProjectButton projectLink="/partyrock" projectName="Clone/Google Search" projectYear="2021" githubLink="" />
+          <ProjectButton projectLink="/partyrock" projectName="Clone/Tesla" projectYear="2021" githubLink="" />
+          <ProjectButton projectLink="/partyrock" projectName="Clone/Instagram" projectYear="2021" githubLink="" />
+          <ProjectButton projectLink="/partyrock" projectName="View More On Github..." githubLink="https://github.com/Marinkiet?tab=repositories" />
         </section>
+        
       </section>
+      
 
     </section>
 
