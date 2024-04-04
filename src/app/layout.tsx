61212f4c332507080f94a7ccbc5cf77721f22fdc
Marinkie <tmarinkie12@gmail.com>
2024-04-05 01:02:49 +0200

    Fix: prettier space removal

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Footer from "@/components/Footer";
-import {ThemeProvider} from "next-themes"
+import { ThemeProvider } from "next-themes"
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -21,18 +21,18 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-  
-    
+
+
       <body className={inter.className} >
         {/* max-w-3xl */}
         <ThemeProvider attribute="class">
-        <main className="mx-20 px-10 py-10 ">
-          {children}
-        </main>
-        <Footer/>
+          <main className="mx-20 px-10 py-10 ">
+            {children}
+          </main>
+          <Footer />
         </ThemeProvider>
       </body>
-      
+
 
     </html>
   );
