01a93226493f687677de52a8c95bbc1389993dc6
Marinkie <tmarinkie12@gmail.com>
2024-04-05 01:01:20 +0200

    Fix: removed me image import

--- src/app/chat/page.tsx
+++ src/app/chat/page.tsx
@@ -2,9 +2,8 @@ import { H1 } from "@/components/UI/H1";
 import { H2 } from "@/components/UI/H2";
 import { H3 } from "@/components/UI/H3";
 import { Metadata } from "next";
-import Image from "next/image";
-import me from '../assets/me.jpeg'
 import Navbar from "@/components/Navbar";
+import Image from "next/image";
 
 //page specifuic metadata
 export const metadata: Metadata = {
@@ -14,14 +13,14 @@ export const metadata: Metadata = {
 export default function Chat() {
   return (
     <section className=" space-y-16 px-1 py-8 flex justify-center flex-col sm:flex-row-reverse sm:justify-between md:flex-col">
-    {/* for mobile set 1 column then 670px and obove set 2 column layout  */}
-    <section className="grid-cols-1 items-center gap-8 sm:grid-cols-2">
-      <div className="  space-y-3 mb-5  bg-[url('/background.png')] bg-no-repeat bg-cover bg-center">
-        <H1 className="  text-center sm:text-start">Hello, I&apos;m Marinkie Thupi</H1>
-        <p className="  text-center sm:text-start">Full Stack Developer: No layer left <span className="text-purple-500">unturned.</span></p>
-        <Navbar />
-      </div>
-    </section>
+      {/* for mobile set 1 column then 670px and obove set 2 column layout  */}
+      <section className="grid-cols-1 items-center gap-8 sm:grid-cols-2">
+        <div className="  space-y-3 mb-5  bg-[url('/background.png')] bg-no-repeat bg-cover bg-center">
+          <H1 className="  text-center sm:text-start">Hello, I&apos;m Marinkie Thupi</H1>
+          <p className="  text-center sm:text-start">Full Stack Developer: No layer left <span className="text-purple-500">unturned.</span></p>
+          <Navbar />
+        </div>
+      </section>
     </section>
 
   );
