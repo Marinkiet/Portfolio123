6c36d16b4b24ca2ad6460260b690ce0b43a76e19
Marinkie <tmarinkie12@gmail.com>
2024-04-04 17:36:40 +0200

    Fix: removed comments and added props to button

--- src/components/UI/ProjectButton.tsx
+++ src/components/UI/ProjectButton.tsx
@@ -5,17 +5,19 @@ import { Github } from 'lucide-react';
 
 
 export default function ProjectButton(props:any) {
-  // const [isVideoVisible, setIsVideoVisible] = useState(false);
 
-  // const handleButtonClick = () => {
-  //   setIsVideoVisible(true);
-  // };
+
   return (
-    <section className="flex justify-center items-center px-1">
+    <section className="flex justify-center items-center px-6 w-fit ">
       <a href={props.githubLink} target="_blank" rel="noopener noreferrer">
   <Github size={30} color="#9ca3af" className="hover:cursor-pointer hover:bg-slate-200 rounded-full p-1"  />
 </a>
-      <h2 className="text-zinc-600 hover:rounded-lg hover:shadow-md px-5 py-1 hover:bg-white hover:cursor-pointer">{props.projectName}<span className="text-gray-400"> {props.projectYear}</span></h2>
+<a
+        href={props.projectLink}
+        className={`text-zinc-600 hover:rounded-lg hover:shadow-md px-5 py-1 hover:bg-white hover:cursor-pointer ${props.nextStyle}`}>
+        {props.projectName}
+        <span className="text-gray-400"> {props.projectYear}</span>
+      </a>
       
     </section>
   );
